83d80bca77e76591f3f69da7a7692c818ff08532
Gosuke Miyashita <gosukenator@gmail.com>
2014-03-29 17:29:36 +0900

    Add tests for comman type

---
+++ spec/command_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe command('cat /tmp/test.sh') do
+  it { should return_stdout 'test' }
+  it { should return_exit_status 0 }
+  its(:stdout) { should match /test/ }
+end
+
+describe command('cat /not_exist') do
+  it { should return_stderr /No such file or directory/ }
+  it { should return_exit_status 1 }
+end
