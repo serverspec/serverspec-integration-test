0261116be649f8161b6c8920fdca897830fe926e
Gosuke Miyashita <gosukenator@gmail.com>
2014-04-25 00:28:09 +0900

    Add package spec

---
+++ spec/package_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+version = backend.get_package_version('chef').stdout
+
+describe package('chef') do
+  it { should     be_installed }
+  it { should     be_installed.with_version(version) }
+  it { should_not be_installed.with_version('0.0.0.0') }
+end
+
+describe package('foo') do
+  it { should_not be_installed }
+end
