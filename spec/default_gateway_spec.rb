ea7af5953ef06f67ab1482e0be6118edad0b0587
Gosuke Miyashita <gosukenator@gmail.com>
2014-04-27 18:20:45 +0900

    Add default gateway spec

---
+++ spec/default_gateway_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+res = backend.run_command('netstat -rn | grep ^0.0.0.0').stdout.split(/\s+/)
+
+describe default_gateway do
+  its(:ipaddress) { should eq res[1] }
+  its(:interface) { should eq res[7] }
+end
