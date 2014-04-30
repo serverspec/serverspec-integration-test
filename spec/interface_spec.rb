d3c54450c59a17047e6222307c3d0abbead90cc8
Gosuke Miyashita <gosukenator@gmail.com>
2014-05-01 02:35:39 +0900

    Add interface spec

---
+++ spec/interface_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+speed  = backend.get_interface_speed_of('eth0').stdout.strip.to_i
+ipaddr = backend.run_command("ip addr show eth0 | grep 'inet ' | awk '{print $2}'").stdout.strip
+
+describe interface('eth0') do
+  its(:speed) { should eq speed }
+  it { should have_ipv4_address(ipaddr) }
+end
