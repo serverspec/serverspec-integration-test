562b2aacbaaa0529b73496b0f2337afd3e96b3d4
Gosuke Miyashita <gosukenator@gmail.com>
2014-05-01 00:16:10 +0900

    Add host spec

--- spec/host_spec.rb
+++ spec/host_spec.rb
@@ -3,4 +3,13 @@ require 'spec_helper'
 describe host('example.jp') do
   it { should be_resolvable }
   its(:ipaddress) { should eq '1.2.3.4' }
+
+  it { should     be_resolvable.by('hosts') }
+  it { should_not be_resolvable.by('dns')   }
+end
+
+describe host('www.google.com') do
+  it { should     be_resolvable             }
+  it { should     be_resolvable.by('dns')   }
+  it { should_not be_resolvable.by('hosts') }
 end
