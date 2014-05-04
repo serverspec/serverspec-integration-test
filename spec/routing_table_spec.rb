17e31f75c19b521217a2a6febcff880116a5eb16
Gosuke Miyashita <gosukenator@gmail.com>
2014-05-05 00:21:55 +0900

    Add routing_table spec

---
+++ spec/routing_table_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+res = backend.run_command('netstat -rn | grep ^0.0.0.0').stdout.split(/\s+/)
+
+describe routing_table do
+  it do
+    should have_entry(
+      :destination => 'default',
+      :interface   => res[7],
+      :gateway     => res[1],
+    )
+  end
+end
