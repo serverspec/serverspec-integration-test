ffe687ac1ffd5987e48cd5266a22e69bee441f83
Gosuke Miyashita <gosukenator@gmail.com>
2014-06-08 02:25:07 +0900

    Install latest beta version

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 if [ $WERCKER_GIT_REPOSITORY = 'specinfra' ]; then
   rake build
   sudo gem install pkg/specinfra-*.gem --no-ri --no-rdoc
-  sudo gem install serverspec -v 2.0.0.beta3 --no-ri --no-rdoc
+  sudo gem install serverspec --pre --no-ri --no-rdoc
 elif [ $WERCKER_GIT_REPOSITORY = 'serverspec' ]; then
   rake build
-  sudo gem install specinfra -v 2.0.0.beta3 --no-ri --no-rdoc
+  sudo gem install specinfra --pre --no-ri --no-rdoc
   sudo gem install pkg/serverspec-*.gem --no-ri --no-rdoc
 else
-  sudo gem install specinfra -v 2.0.0.beta3 --no-ri --no-rdoc
-  sudo gem install serverspec -v 2.0.0.beta3 --no-ri --no-rdoc
+  sudo gem install specinfra --pre --no-ri --no-rdoc
+  sudo gem install serverspec --pre --no-ri --no-rdoc
 fi
 
 cd $WORKING_DIR
