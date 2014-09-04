56b8b12d6e9efbfef37053083827bb11440bde05
Gosuke Miyashita <gosukenator@gmail.com>
2014-09-04 10:39:17 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@
 if [ $WERCKER_GIT_REPOSITORY = 'specinfra' ]; then
   rake build
   sudo gem install pkg/specinfra-*.gem --no-ri --no-rdoc
-  sudo gem install serverspec --no-ri --no-rdoc
+  sudo gem install serverspec --pre --no-ri --no-rdoc
 elif [ $WERCKER_GIT_REPOSITORY = 'serverspec' ]; then
   rake build
+  sudo gem install specinfra --pre --no-ri --no-rdoc
   sudo gem install pkg/serverspec-*.gem --no-ri --no-rdoc
 else
-  sudo gem install serverspec --no-ri --no-rdoc
+  sudo gem install specinfra --pre --no-ri --no-rdoc
+  sudo gem install serverspec --pre --no-ri --no-rdoc
 fi
 
 cd $WORKING_DIR
 
-berks vendor cookbooks
+bundle install
+
