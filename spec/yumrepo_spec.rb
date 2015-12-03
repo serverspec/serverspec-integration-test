f27ca7c631eb5ae212c5ff22c38dc0b106676a9c
Gosuke Miyashita <gosukenator@gmail.com>
2015-12-03 09:53:20 +0900

    Fix to run tests only on RedHat

--- spec/yumrepo_spec.rb
+++ spec/yumrepo_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
-describe yumrepo('extras') do
-  it { should be_exist   }
-  it { should be_enabled }
+if os[:family] == 'redhat'
+  describe yumrepo('extras') do
+    it { should be_exist   }
+    it { should be_enabled }
+  end
+
+  describe yumrepo('C6.0') do
+    it { should_not exist }
+  end
 end
 
-describe yumrepo('C6.0') do
-  it { should_not exist }
-end
