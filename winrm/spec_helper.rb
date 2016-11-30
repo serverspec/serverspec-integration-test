366ca47cde89ab7c32f8329d8d96339fb990b932
sawanoboly <sawanoboriyu@higanworks.com>
2016-11-30 19:19:54 +0900

    remind 1.9.3 helper

--- winrm/spec_helper.rb
+++ winrm/spec_helper.rb
@@ -3,10 +3,20 @@ require 'winrm'
 
 set :backend, :winrm
 
-opts = {
-  endpoint: 'http://127.0.0.1:5985/wsman',
-  user: 'appveyor',
-  password: ENV['WINDOWS_PASSWORD'],
-  operation_timeout: 300 # 5 minutes max timeout for any operation
-}
-Specinfra.configuration.winrm = WinRM::Connection.new(opts)
+if RbConfig::CONFIG['MAJOR'] == "1"
+  user = 'appveyor'
+  pass = ENV['WINDOWS_PASSWORD']
+  endpoint = "http://127.0.0.1:5985/wsman"
+
+  winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => user, :pass => pass, :basic_auth_only => true)
+  winrm.set_timeout 300 # 5 minutes max timeout for any operation
+  Specinfra.configuration.winrm = winrm
+else
+  opts = {
+    endpoint: 'http://127.0.0.1:5985/wsman',
+    user: 'appveyor',
+    password: ENV['WINDOWS_PASSWORD'],
+    operation_timeout: 300 # 5 minutes max timeout for any operation
+  }
+  Specinfra.configuration.winrm = WinRM::Connection.new(opts)
+end
