1fc6f2fb047ff4b47b20049e980f08a27f853cf6
Gosuke Miyashita <gosukenator@gmail.com>
2017-03-29 18:05:52 +0900

    Migrate from wercker classic

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ $WERCKER_GIT_REPOSITORY = 'specinfra' ]; then
+if [ "$WERCKER_GIT_REPOSITORY" = 'specinfra' ]; then
   rake build
-  sudo gem install pkg/specinfra-*.gem --no-ri --no-rdoc
-  sudo gem install serverspec --pre --no-ri --no-rdoc
-elif [ $WERCKER_GIT_REPOSITORY = 'serverspec' ]; then
+  gem install pkg/specinfra-*.gem --no-ri --no-rdoc
+  gem install serverspec --pre --no-ri --no-rdoc
+elif [ "$WERCKER_GIT_REPOSITORY" = 'serverspec' ]; then
   rake build
-  sudo gem install specinfra --pre --no-ri --no-rdoc
-  sudo gem install pkg/serverspec-*.gem --no-ri --no-rdoc
+  gem install specinfra --pre --no-ri --no-rdoc
+  gem install pkg/serverspec-*.gem --no-ri --no-rdoc
 else
-  sudo gem install specinfra --pre --no-ri --no-rdoc
-  sudo gem install serverspec --pre --no-ri --no-rdoc
+  gem install specinfra --pre --no-ri --no-rdoc
+  gem install serverspec --pre --no-ri --no-rdoc
 fi
 
 cd $WORKING_DIR
