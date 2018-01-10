2a5df742a83ca267bb456ca5184929d2cd377c92
Gosuke Miyashita <gosukenator@gmail.com>
2018-01-10 16:27:21 +0900

    Add a test to cron resource

--- spec/cron_spec.rb
+++ spec/cron_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe cron do
   it { should have_entry '* * * * * ls /tmp' }
   it { should have_entry('* * * * * ls /tmp').with_user('daemon') }
+  its(:table) { should match  /\* \* \* \* \* ls \/tmp/ }
 end
