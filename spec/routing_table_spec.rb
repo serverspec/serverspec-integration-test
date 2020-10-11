8d103e143dcaf6665bdd0b621967bfdeedfb0a89
Gosuke Miyashita <gosukenator@gmail.com>
2020-10-11 11:55:33 +0900

    Pending routing related specs

--- spec/routing_table_spec.rb
+++ spec/routing_table_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 res = Specinfra::Runner.run_command('netstat -rn | grep ^0.0.0.0').stdout.split(/\s+/)
 
 describe routing_table do
-  it do
+  xit do
     should have_entry(
       :destination => 'default',
       :interface   => res[7],
